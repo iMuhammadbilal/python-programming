8aa1f8e2382ca109c0de192bd6cdd9b3ea181821
bilal-marwat <bilalmarwat134@gmail.com>
2025-07-20 11:27:01 +0500

    assignment variables releated

---
+++ Variables/AssignmentVariable.py
@@ -0,0 +1,62 @@
+#  ------- ------------ ------------- 1 Assignment variable in python
+
+
+# declare a variable name and assign value your name
+
+# declare age variable and assign your age
+
+# declare city variable and assign your city
+
+# then print all three variables in one line like this:
+
+# print("My name is", name, ", I am", age, "years old, 
+
+my_name = "Muhammad Bilal"
+my_age = 21
+my_city = "Lakki Marwat"
+
+print("My Name is", my_name,"I am", my_age, "years old, and I live in", my_city + ".")
+
+
+#  ------- ------------ ------------- 2 Assignment variable in python
+
+# change the value of varaible updata age variable to your new age
+my_age = 22
+# then print the updated age variable
+print("Updated age is", my_age)
+
+
+# ------- ------------ ------------- 3 assignment variable in python
+
+# Multiple variables and swapping
+
+x = 5
+y = 10
+# before swapping
+print("Before swapping x =", x, "and y =", y)
+# Swapping values
+x, y = y, x
+# after swapping
+print("After swapping x =", x, "and y =", y)
+
+# benefit of this method is that it allows us to swap values without using a temporary variable.
+# This is a concise and efficient way to swap values in Python.
+
+
+
+# ------- ------------ ------------- 4 assignment variable in python
+
+# Combine strings
+
+my_first_name = "Muhammad"
+my_last_name = "Bilal"
+my_full_name = my_first_name + " " + my_last_name
+print("My full name is", my_full_name)
+
+
+#----------- ------------ ------------- 5 assignment variable in python
+
+
+numbers_of_apples = 100
+apples = "apples"
+print("I have", numbers_of_apples, apples)
---
+++ Variables/pythonVariables1.py
@@ -0,0 +1,38 @@
+# Variable in pythonVariables1.py
+
+# what is variable in python
+
+# A variable in Python is a reserved memory location to store values.
+# Variables are created when you assign a value to them. They can hold different types of data, such as numbers, strings, lists, and more.
+
+# Variables are how we store data as our program runs.
+# We can create a variable by assigning a value to it.
+
+
+#In short a variable is like a memory unit that we can access by typing the name of the variable. 
+
+#Each variable has a unique name and a value that can be of different types.
+
+
+#creating a variable
+
+#A "variable" is just a name that we give to a value. For example, we can make a new variable named my_name and set its value to "Bilal":
+
+my_name = "Bilal"
+
+
+# We have the freedom to choose any name for our variables, but they should be descriptive and consist of a single "token", meaning continuous text with underscores separating the words.
+
+# USING VARIABLES
+
+
+# Once we have a variable, we can access its value by using its name. For example, this will print
+
+print(my_name)
+
+
+# Assigement about variables?
+# Let's create a variable to store our age and print it:
+my_age = 25
+print(my_age)
+
