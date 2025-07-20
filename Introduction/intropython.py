602d53d83499b87f2291ab799c7b672a09e3b4d0
bilal-marwat <bilalmarwat134@gmail.com>
2025-07-20 10:44:48 +0500

    added introduction folder with initial files

---
+++ Introduction/intropython.py
@@ -0,0 +1,17 @@
+# Introduction to Python
+
+#Python is a high-level, interpreted programming language known for its simplicity and readability. It supports multiple programming paradigms, including procedural, object-oriented, and functional programming. Python is widely used for web development, data analysis, artificial intelligence, scientific computing, and more.
+
+#Python is one of the world's easiest and most popular programming languages. This course will teach you the basics of Python, without requiring any prior knowledge.
+
+#In Python, we use print() to show words on the screen. The words go inside quotation marks.
+
+print("Hello, World!")  # Output: Hello, World!
+
+
+# Note that anything inside quotation marks is case sensitive. For example:
+
+print("hello, world!")  # Output: hello, world!
+
+
+
